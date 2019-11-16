9b9cb480844769bedd48e4245d1f737abe505c8e
Joel Tolvanen <joel.tolvanen@xiple.io>
2019-11-16 12:44:29 +0200

    Component for Mixed Chart and moving KPI to charts folder

--- src/components/Headers/Header.jsx
+++ src/components/Headers/Header.jsx
@@ -19,7 +19,7 @@ import React from "react";
 
 // reactstrap components
 import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
-import KPI from "../KPI";
+import KPI from "../Charts/KPI";
 
 class Header extends React.Component {
   render() {
