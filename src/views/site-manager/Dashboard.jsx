0bb0234161a52c416bd1a2da92fac2ec91330cc1
papivaan <vaananen.paavo@gmail.com>
2019-11-16 22:38:57 +0200

    add templates for site manager

--- src/views/site-manager/Dashboard.jsx
+++ src/views/site-manager/Dashboard.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import {
-  Container, Row, Col, Card,
+  Container,
+  Row,
+  Button,
+  Card,
   CardHeader,
-  CardFooter, Table,
+  CardFooter,
+  Table,
+  Input,
+  Badge
 } from "reactstrap";
 
 import Header from "../../components/Headers/Header.jsx";
 
+const initialTask = {
+  orderId: '',
+  description: '',
+  employee: ''
+}
+
 const Dashboard = ({ match, location }) => {
+  const [isCreating, setCreate] = useState(false)
+  const [task, setTask] = useState({ ...initialTask })
+  console.log(task)
   return (
     <>
       <Header />
@@ -21,11 +36,10 @@ const Dashboard = ({ match, location }) => {
               <Table className="align-items-center table-flush" responsive>
                 <thead className="thead-light">
                   <tr>
-                    <th scope="col">Project</th>
-                    <th scope="col">Order</th>
-                    <th scope="col">Status</th>
+                    <th scope="col">Order_ID</th>
+                    <th scope="col">Description</th>
                     <th scope="col">Assignee</th>
-                    <th scope="col">Completion</th>
+                    <th scope="col">Status</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -34,10 +48,63 @@ const Dashboard = ({ match, location }) => {
                     <td>Placeholder</td>
                     <td>Placeholder</td>
                     <td>Placeholder</td>
-                    <td>Placeholder</td>
                   </tr>
+                  {isCreating && (
+                    <tr>
+                      <td>
+                        <Input
+                          type="select"
+                          value={task.orderId}
+                          onChange={(e) => setTask({ ...task, orderId: e.target.value })}
+                          placeholder="Select order ID">
+                          {/* map orderIds here */}
+                          <option>1234567</option>
+                          <option>12341245167</option>
+                          <option>12345fsgag7</option>
+                          <option>1234qwert67</option>
+                        </Input>
+                      </td>
+                      <td>
+                        <Input
+                          type="textarea"
+                          placeholder="Description of the task"
+                          value={task.description}
+                          onChange={(e) => setTask({ ...task, description: e.target.value })} />
+                      </td>
+                      <td>
+                        <Input
+                          type="select"
+                          placeholder="Employee"
+                          value={task.employee}
+                          onChange={(e) => setTask({ ...task, employee: e.target.value })}
+                        >
+                          {/* map employee names */}
+                          <option>Ake</option>
+                          <option>Make</option>
+                          <option>Pera</option>
+                          <option>Mä</option>
+                        </Input>
+                      </td>
+                      <td><Badge style={{ width: '80%' }} color="info">pending</Badge></td>
+                    </tr>
+                  )}
                 </tbody>
               </Table>
+              <CardFooter style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                {isCreating && (<Button color="primary" style={{ marginRight: '10px' }} onClick={() => setCreate(false)}>Cancel</Button>)}
+                <Button
+                  color="info"
+                  disabled={
+                    isCreating &&
+                    task.description.length < 5 &&
+                    task.employee.length < 1 &&
+                    task.orderId.length < 1
+                  }
+                  onClick={() =>
+                    isCreating ?
+                      console.log(`Creating task: ${JSON.stringify(task, null, 2)}`) :
+                      setCreate(true)}>Create new task</Button>
+              </CardFooter>
             </Card>
           </div>
         </Row>
