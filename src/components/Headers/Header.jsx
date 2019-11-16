327d1a0c143532c6b7927d7196c17f3272c7e63a
papivaan <vaananen.paavo@gmail.com>
2019-11-16 17:42:54 +0200

    implement stuupid yet simple civil page

--- src/components/Headers/Header.jsx
+++ src/components/Headers/Header.jsx
@@ -21,49 +21,51 @@ import React from "react";
 import { Container, Row } from "reactstrap";
 import KPI from "../Charts/KPI";
 
-const Header = () => {
+const Header = ({ isCivil }) => {
   return (
     <>
       <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
-        <Container fluid>
-          <div className="header-body">
-            {/* Card stats */}
-            <Row>
-              <KPI
-                title="Eka"
-                value="100.00"
-                color="red"
-                secondaryTitle="Viime kuussa"
-                secondaryValue="5000000"
-                icon={<i className="fas fa-chart-pie" />}
-              />
-              <KPI
-                title="New users"
-                value="2,356"
-                color="orange"
-                secondaryTitle="Since last week"
-                secondaryValue="3.48%"
-                icon={<i className="fas fa-chart-bar" />}
-              />
-              <KPI
-                title="Employees"
-                value="924"
-                color="green"
-                secondaryTitle="Since yesterday"
-                secondaryValue="1.10%"
-                icon={<i className="fas fa-users" />}
-              />
-              <KPI
-                title="Performance"
-                value="69.5%"
-                color="blue"
-                secondaryTitle="Since last month"
-                secondaryValue="12%"
-                icon={<i className="fas fa-percent" />}
-              />
-            </Row>
-          </div>
-        </Container>
+        {!isCivil && (
+          <Container fluid>
+            <div className="header-body">
+              {/* Card stats */}
+              <Row>
+                <KPI
+                  title="Eka"
+                  value="100.00"
+                  color="red"
+                  secondaryTitle="Viime kuussa"
+                  secondaryValue="5000000"
+                  icon={<i className="fas fa-chart-pie" />}
+                />
+                <KPI
+                  title="New users"
+                  value="2,356"
+                  color="orange"
+                  secondaryTitle="Since last week"
+                  secondaryValue="3.48%"
+                  icon={<i className="fas fa-chart-bar" />}
+                />
+                <KPI
+                  title="Employees"
+                  value="924"
+                  color="green"
+                  secondaryTitle="Since yesterday"
+                  secondaryValue="1.10%"
+                  icon={<i className="fas fa-users" />}
+                />
+                <KPI
+                  title="Performance"
+                  value="69.5%"
+                  color="blue"
+                  secondaryTitle="Since last month"
+                  secondaryValue="12%"
+                  icon={<i className="fas fa-percent" />}
+                />
+              </Row>
+            </div>
+          </Container>
+        )}
       </div>
     </>
   );
