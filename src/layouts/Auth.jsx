08196eef42fd7f553c907ea7a8533bde3da2ccf2
papivaan <vaananen.paavo@gmail.com>
2019-11-16 15:34:41 +0200

    cleanup landing page

--- src/layouts/Auth.jsx
+++ src/layouts/Auth.jsx
@@ -52,16 +52,14 @@ class Auth extends React.Component {
     return (
       <>
         <div className="main-content">
-          <AuthNavbar />
           <div className="header bg-gradient-info py-7 py-lg-8">
             <Container>
               <div className="header-body text-center mb-7">
                 <Row className="justify-content-center">
                   <Col lg="5" md="6">
-                    <h1 className="text-white">Welcome!</h1>
+                    <h1 className="text-white">Welcome oke-app!</h1>
                     <p className="text-lead text-light">
-                      Use these awesome forms to login or create new account in
-                      your project for free.
+                      Please select role below to try our platform.
                     </p>
                   </Col>
                 </Row>
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -101,26 +101,6 @@ const Login = () => {
             </Row>
           </CardBody>
         </Card>
-        <Row className="mt-3">
-          <Col xs="6">
-            <a
-              className="text-light"
-              href="#pablo"
-              onClick={e => e.preventDefault()}
-            >
-              <small>Forgot password?</small>
-            </a>
-          </Col>
-          <Col className="text-right" xs="6">
-            <a
-              className="text-light"
-              href="#pablo"
-              onClick={e => e.preventDefault()}
-            >
-              <small>Create new account</small>
-            </a>
-          </Col>
-        </Row>
       </Col>
     </>
   );
