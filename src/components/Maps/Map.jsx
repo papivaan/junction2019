ddeaf882815b21c072a50364f20debf068cea49d
papivaan <vaananen.paavo@gmail.com>
2019-11-16 02:05:43 +0200

    git add sites to map, refactor map component

---
+++ src/components/Maps/Map.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+
+import {
+	withScriptjs,
+	withGoogleMap,
+	GoogleMap,
+	Marker
+} from "react-google-maps";
+
+import { MarkerClusterer } from "react-google-maps/lib/components/addons/MarkerClusterer";
+
+import config from "./config";
+
+const Map = ({ data }) => {
+	const MapWrapper = withScriptjs(
+		withGoogleMap(props => (
+			<GoogleMap
+				defaultZoom={11}
+				defaultCenter={{ lat: 60.2221845, lng: 24.9896104 }}
+				defaultOptions={{
+					scrollwheel: false,
+					styles: [
+						{
+							featureType: "administrative",
+							elementType: "labels.text.fill",
+							stylers: [{ color: "#444444" }]
+						},
+						{
+							featureType: "landscape",
+							elementType: "all",
+							stylers: [{ color: "#f2f2f2" }]
+						},
+						{
+							featureType: "poi",
+							elementType: "all",
+							stylers: [{ visibility: "off" }]
+						},
+						{
+							featureType: "road",
+							elementType: "all",
+							stylers: [{ saturation: -100 }, { lightness: 45 }]
+						},
+						{
+							featureType: "road.highway",
+							elementType: "all",
+							stylers: [{ visibility: "simplified" }]
+						},
+						{
+							featureType: "road.arterial",
+							elementType: "labels.icon",
+							stylers: [{ visibility: "off" }]
+						},
+						{
+							featureType: "transit",
+							elementType: "all",
+							stylers: [{ visibility: "off" }]
+						},
+						{
+							featureType: "water",
+							elementType: "all",
+							stylers: [{ color: "#5e72e4" }, { visibility: "on" }]
+						}
+					]
+				}}
+			>
+				<MarkerClusterer
+					onClick={() => console.log('pööö')}
+					averageCenter
+					enableRetinaIcons
+					gridSize={60}
+				>
+					{props.sites.map(site => (
+						<Marker key={site.order} position={site.location} />
+					)
+					)}
+				</MarkerClusterer>
+			</GoogleMap>
+		))
+	);
+	return (
+		<MapWrapper
+			googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${config.googleMapsApiKey}`}
+			loadingElement={<div style={{ height: `100%` }} />}
+			sites={data.filter(d => !!d.location)}
+			containerElement={
+				<div
+					style={{ height: `600px` }}
+					className="map-canvas"
+					id="map-canvas"
+				/>
+			}
+			mapElement={
+				<div style={{ height: `100%`, borderRadius: "inherit" }} />
+			}
+		/>
+	)
+}
+
+export default Map;
--- src/views/examples/Maps.jsx
+++ src/views/examples/Maps.jsx
@@ -16,110 +16,34 @@
 
 */
 import React from "react";
-// react plugin used to create google maps
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
 
 // reactstrap components
 import { Card, Container, Row } from "reactstrap";
 
-// Config
-import config from "../../config";
+import Map from '../../components/Maps/Map'
+
+import { useData } from "../../contexts/data-context"
 
 // core components
 import Header from "../../components/Headers/Header.jsx";
-// mapTypeId={google.maps.MapTypeId.ROADMAP}
-const MapWrapper = withScriptjs(
-  withGoogleMap(props => (
-    <GoogleMap
-      defaultZoom={12}
-      defaultCenter={{ lat: 40.748817, lng: -73.985428 }}
-      defaultOptions={{
-        scrollwheel: false,
-        styles: [
-          {
-            featureType: "administrative",
-            elementType: "labels.text.fill",
-            stylers: [{ color: "#444444" }]
-          },
-          {
-            featureType: "landscape",
-            elementType: "all",
-            stylers: [{ color: "#f2f2f2" }]
-          },
-          {
-            featureType: "poi",
-            elementType: "all",
-            stylers: [{ visibility: "off" }]
-          },
-          {
-            featureType: "road",
-            elementType: "all",
-            stylers: [{ saturation: -100 }, { lightness: 45 }]
-          },
-          {
-            featureType: "road.highway",
-            elementType: "all",
-            stylers: [{ visibility: "simplified" }]
-          },
-          {
-            featureType: "road.arterial",
-            elementType: "labels.icon",
-            stylers: [{ visibility: "off" }]
-          },
-          {
-            featureType: "transit",
-            elementType: "all",
-            stylers: [{ visibility: "off" }]
-          },
-          {
-            featureType: "water",
-            elementType: "all",
-            stylers: [{ color: "#5e72e4" }, { visibility: "on" }]
-          }
-        ]
-      }}
-    >
-      <Marker position={{ lat: 40.748817, lng: -73.985428 }} />
-    </GoogleMap>
-  ))
-);
 
-class Maps extends React.Component {
-  render() {
-    return (
-      <>
-        <Header />
-        {/* Page content */}
-        <Container className="mt--7" fluid>
-          <Row>
-            <div className="col">
-              <Card className="shadow border-0">
-                <MapWrapper
-                  googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${config.googleMapsApiKey}`}
-                  loadingElement={<div style={{ height: `100%` }} />}
-                  containerElement={
-                    <div
-                      style={{ height: `600px` }}
-                      className="map-canvas"
-                      id="map-canvas"
-                    />
-                  }
-                  mapElement={
-                    <div style={{ height: `100%`, borderRadius: "inherit" }} />
-                  }
-                />
-              </Card>
-            </div>
-          </Row>
-        </Container>
-      </>
-    );
-  }
+const Maps = () => {
+  const { data } = useData();
+  return (
+    <>
+      <Header />
+      {/* Page content */}
+      <Container className="mt--7" fluid>
+        <Row>
+          <div className="col">
+            <Card className="shadow border-0">
+              <Map data={data} />
+            </Card>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default Maps;
