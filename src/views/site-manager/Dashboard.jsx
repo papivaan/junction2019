64b0d1dfcb6d73747500f3105b09b4520c9d1501
papivaan <vaananen.paavo@gmail.com>
2019-11-17 01:42:27 +0200

    add tasks saatana

--- src/views/site-manager/Dashboard.jsx
+++ src/views/site-manager/Dashboard.jsx
@@ -1,37 +1,40 @@
 import React, { useState } from "react";
-import {
-  Container,
-  Row,
-  Card
-} from "reactstrap";
+import { Container, Row, Card } from "reactstrap";
 
 import Header from "../../components/Headers/Header.jsx";
-import Map from '../../components/Maps/Map';
-import TaskCard from './TaskCard'
+import Map from "../../components/Maps/Map";
+import TaskCard from "./TaskCard";
 
-import { useData } from "../../contexts/data-context"
-import _ from "lodash"
+import { useData } from "../../contexts/data-context";
+import _ from "lodash";
 
 const Dashboard = ({ match, location }) => {
-  console.log(match.params.id)
   const { data } = useData();
-  const dataBySites = _.values(_.groupBy(data.filter(d => !!d.location && d.supervisor === match.params.id), "location.lat"));
+  const dataBySites = _.values(
+    _.groupBy(
+      data.filter(d => !!d.location && d.supervisor === match.params.id),
+      "location.lat"
+    )
+  ).slice(300);
+  const orders = _.flatMap(dataBySites, d => d).map(d => ({
+    text: d.short_text,
+    id: d.order
+  }));
 
   return (
     <>
       <Header />
       <Container className="mt--7" fluid>
-      <Row style={{ marginBottom: '20px' }}>
-          <TaskCard tasks={undefined} onCreateTask={(task) => console.log(task)}/>
+        <Row style={{ marginBottom: "20px" }}>
+          <TaskCard orders={orders} />
         </Row>
         <Row>
-            <div className="col">
+          <div className="col">
             <Card className="shadow border-0">
               <Map data={dataBySites} />
             </Card>
           </div>
         </Row>
-        
       </Container>
     </>
   );
