bc10d7f6261414fe0c3d40e3788851752e7f4b56
papivaan <vaananen.paavo@gmail.com>
2019-11-17 02:18:02 +0200

    restrict civil map view

--- src/components/Maps/Map.jsx
+++ src/components/Maps/Map.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 
-import {
-	withScriptjs,
-	withGoogleMap,
-	GoogleMap,
-} from "react-google-maps";
+import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
 
 import { MarkerClusterer } from "react-google-maps/lib/components/addons/MarkerClusterer";
 
@@ -12,92 +8,91 @@ import config from "./config";
 
 import MarkerWithInfoWindow from "./MarkerWithInfoWindow";
 
-const Map = ({ data }) => {
-	const MapWrapper = withScriptjs(
-		withGoogleMap(props => (
-			<GoogleMap
-				defaultZoom={11}
-				defaultCenter={{ lat: 60.2221845, lng: 24.9896104 }}
-				defaultOptions={{
-					scrollwheel: false,
-					styles: [
-						{
-							featureType: "administrative",
-							elementType: "labels.text.fill",
-							stylers: [{ color: "#444444" }]
-						},
-						{
-							featureType: "landscape",
-							elementType: "all",
-							stylers: [{ color: "#f2f2f2" }]
-						},
-						{
-							featureType: "poi",
-							elementType: "all",
-							stylers: [{ visibility: "off" }]
-						},
-						{
-							featureType: "road",
-							elementType: "all",
-							stylers: [{ saturation: -100 }, { lightness: 45 }]
-						},
-						{
-							featureType: "road.highway",
-							elementType: "all",
-							stylers: [{ visibility: "simplified" }]
-						},
-						{
-							featureType: "road.arterial",
-							elementType: "labels.icon",
-							stylers: [{ visibility: "off" }]
-						},
-						{
-							featureType: "transit",
-							elementType: "all",
-							stylers: [{ visibility: "off" }]
-						},
-						{
-							featureType: "water",
-							elementType: "all",
-							stylers: [{ color: "#5e72e4" }, { visibility: "on" }]
-						}
-					]
-				}}
-			>
-				<MarkerClusterer
-					averageCenter
-					enableRetinaIcons
-					gridSize={60}
-				>
-					{props.sites.map((site, i) => {
-						const key = site.find(s => s.short_text).short_text;
-						const location = site.find(s => s.location).location;
-						return (
-							<MarkerWithInfoWindow key={key} name={key} location={location} site={site} />
-						)
-					}
-					)}
-				</MarkerClusterer>
-			</GoogleMap>
-		))
-	);
-	return (
-		<MapWrapper
-			googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${config.googleMapsApiKey}`}
-			loadingElement={<div style={{ height: `100%` }} />}
-			sites={data}
-			containerElement={
-				<div
-					style={{ height: `600px` }}
-					className="map-canvas"
-					id="map-canvas"
-				/>
-			}
-			mapElement={
-				<div style={{ height: `100%`, borderRadius: "inherit" }} />
-			}
-		/>
-	)
-}
+const Map = ({ data, isCivil }) => {
+  const MapWrapper = withScriptjs(
+    withGoogleMap(props => (
+      <GoogleMap
+        defaultZoom={11}
+        defaultCenter={{ lat: 60.2221845, lng: 24.9896104 }}
+        defaultOptions={{
+          scrollwheel: false,
+          styles: [
+            {
+              featureType: "administrative",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#444444" }]
+            },
+            {
+              featureType: "landscape",
+              elementType: "all",
+              stylers: [{ color: "#f2f2f2" }]
+            },
+            {
+              featureType: "poi",
+              elementType: "all",
+              stylers: [{ visibility: "off" }]
+            },
+            {
+              featureType: "road",
+              elementType: "all",
+              stylers: [{ saturation: -100 }, { lightness: 45 }]
+            },
+            {
+              featureType: "road.highway",
+              elementType: "all",
+              stylers: [{ visibility: "simplified" }]
+            },
+            {
+              featureType: "road.arterial",
+              elementType: "labels.icon",
+              stylers: [{ visibility: "off" }]
+            },
+            {
+              featureType: "transit",
+              elementType: "all",
+              stylers: [{ visibility: "off" }]
+            },
+            {
+              featureType: "water",
+              elementType: "all",
+              stylers: [{ color: "#5e72e4" }, { visibility: "on" }]
+            }
+          ]
+        }}
+      >
+        <MarkerClusterer averageCenter enableRetinaIcons gridSize={60}>
+          {props.sites.map((site, i) => {
+            const key = site.find(s => s.short_text).short_text;
+            const location = site.find(s => s.location).location;
+            return (
+              <MarkerWithInfoWindow
+                key={key}
+                name={key}
+                location={location}
+                site={site}
+                isCivil={isCivil}
+              />
+            );
+          })}
+        </MarkerClusterer>
+      </GoogleMap>
+    ))
+  );
+  return (
+    <MapWrapper
+      googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${config.googleMapsApiKey}`}
+      loadingElement={<div style={{ height: `100%` }} />}
+      sites={data}
+      containerElement={
+        <div
+          style={{ height: `600px` }}
+          className="map-canvas"
+          id="map-canvas"
+        />
+      }
+      mapElement={<div style={{ height: `100%`, borderRadius: "inherit" }} />}
+    />
+  );
+};
 
 export default Map;
--- src/views/civil/Dashboard.jsx
+++ src/views/civil/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col, Card } from "reactstrap";
-import { useData } from "../../contexts/data-context"
+import { useData } from "../../contexts/data-context";
 import Header from "../../components/Headers/Header.jsx";
 import Map from "../../components/Maps/Map.jsx";
 
@@ -12,10 +12,12 @@ const Dashboard = ({ match, location }) => {
 
   var filteredData = data.filter(work => work.end_limit_date.length < 1);
 
-  console.log(filteredData);
-  //const filteredData = _.uniqBy(data, "short_text");
-
-  const dataBySites = _.values(_.groupBy(data.filter(d => !!d.location), "location.lat"));
+  const dataBySites = _.values(
+    _.groupBy(
+      data.filter(d => !!d.location),
+      "location.lat"
+    )
+  );
 
   return (
     <>
@@ -24,15 +26,13 @@ const Dashboard = ({ match, location }) => {
         <Row>
           <div className="col">
             <Card className="shadow border-0">
-              <Map data={dataBySites} />
+              <Map data={dataBySites} isCivil />
             </Card>
           </div>
         </Row>
         <Row>
           <Col className="mb-5 mb-xl-0" xl="12">
-            <ProjectTable
-              data={filteredData}
-            />
+            <ProjectTable data={filteredData} />
           </Col>
         </Row>
       </Container>
